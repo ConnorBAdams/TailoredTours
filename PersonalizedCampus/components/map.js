19189a472c6143356680445864dfc0d3aa14bdc2
Connor <connorbadams@gmail.com>
2020-10-08 18:52:41 -0500

    Node Placement Prototype
    First version of node placement working, going to be changing location and getting a deeper implementation later on
    
    Co-Authored-By: Samantha Bagwell <48490674+samanthabagwell@users.noreply.github.com>

--- PersonalizedCampus/components/map.js
+++ PersonalizedCampus/components/map.js
@@ -1,31 +1,54 @@
 import React, { useEffect, useState } from 'react';
-import { StyleSheet, Text, View, Dimensions, ActivityIndicator } from 'react-native';
+import { StyleSheet, Text, View, Dimensions } from 'react-native';
 import MapView from 'react-native-maps';
+import { Marker } from 'react-native-maps';
 import * as Location from 'expo-location'
 import globalStyles from '../styles'
+import Button from '../components/button'
 
 const MapComponent = props => {
-    const [location, setLocation] = useState(null)
-    const [errorMsg, setErrorMsg] = useState(null);
+    const [mapType, setMapType] = useState('standard')
+    const [addNodeEnabled, setAddNodeEnabled] = useState(false)
+    const [nodes, setNodes] = useState([])
 
-    useEffect(() => {
-        (async () => {
-          let { status } = await Location.requestPermissionsAsync();
-          if (status !== 'granted') {
-            setErrorMsg('Permission to access location was denied, is is required for the map');
-          }
-    
-          let location = await Location.getCurrentPositionAsync({});
-          setLocation(location);
-          console.log(location)
-        })();
-      }, []);
+    const toggleEditMode = () => {
+        if (addNodeEnabled) {
+            setAddNodeEnabled(false)
+        } else {
+            setAddNodeEnabled(true)
+        }
+    }
+
+    const createNode = (e) => {
+        if (addNodeEnabled)
+        {
+            if (nodes.length > 0) {
+                setNodes([...nodes, {latitude:e.nativeEvent.coordinate.latitude, longitude:e.nativeEvent.coordinate.longitude}])
+            } else {
+                setNodes([{latitude:e.nativeEvent.coordinate.latitude, longitude:e.nativeEvent.coordinate.longitude}])
+            }
+            console.log(e.nativeEvent.coordinate.latitude, e.nativeEvent.coordinate.longitude, nodes.length)
+        }
+   }
 
     return (
         <View style={styles.container}>
-            {location === null && <ActivityIndicator size="large" />}
-            {location != null &&
-            <MapView style={styles.mapStyle} region={{latitude:location.latitude, longitude:location.longitude}} /> }
+            <MapView style={styles.mapStyle} mapType={mapType}
+            initialRegion={{latitude:props.location.coords.latitude, longitude:props.location.coords.longitude, latitudeDelta: 0.0922, longitudeDelta: 0.0421}} 
+            onPress={e => createNode(e)}> 
+            <Marker key={0} coordinate={{latitude:props.location.coords.latitude, longitude:props.location.coords.longitude}}
+            title="Your Location" description="" />
+            {nodes.map((marker, index) => (
+                <Marker
+                key={index}
+                coordinate={{latitude:marker.latitude, longitude:marker.longitude}}
+                title={'test'}
+                />
+            ))}
+            </MapView>
+            <Button title='Toggle Mode' onPress={() => { if (mapType === 'standard' ) setMapType('satellite'); else setMapType('standard')}} />
+            <Button title='Place Node' onPress={() => toggleEditMode()} />
+
         </View>
     );
 }
@@ -39,7 +62,7 @@ const styles = StyleSheet.create({
       },
       mapStyle: {
         width: Dimensions.get('window').width,
-        height: Dimensions.get('window').height,
+        height: Dimensions.get('window').height * 0.8,
       },
 });
 
