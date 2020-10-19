9d3dc2ca103814c722bd8f2cf1b06c6341aa1afc
Connor <connorbadams@gmail.com>
2020-10-19 00:47:54 -0500

    I moved the config file for security
    Hopefully I did it right this time

--- PersonalizedCampus/App.js
+++ PersonalizedCampus/App.js
@@ -10,18 +10,8 @@ import AccountCreationScreen from './screens/accountCreationScreen';
 import MapScreen from './screens/mapScreen';
 import SignedInScreen from './screens/signedInScreen';
 import * as firebase from 'firebase';
+import firebaseConfig from './config'
 
-// Initialize Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyBeuWLZo7j8Ug0G1G0z9Al1UR7VvQf4M6o",
-  authDomain: "tailoredtours-25201.firebaseapp.com",
-  databaseURL: "https://tailoredtours-25201.firebaseio.com",
-  projectId: "tailoredtours-25201",
-  storageBucket: "tailoredtours-25201.appspot.com",
-  messagingSenderId: "584187795626",
-  appId: "1:584187795626:web:69e2ce253c9c751fd598d3",
-  measurementId: "G-K73Z5DK70V"
-};
 
 const Stack = createStackNavigator();
 
