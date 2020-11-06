9061d026cc09bd09abe335c7bb3d839dae4c1e90
Zac Petro <zmpetro@gmail.com>
2020-11-06 16:48:04 -0600

    add image picker to finalize tour screen

--- PersonalizedCampus/screens/finalizeTourScreen.js
+++ PersonalizedCampus/screens/finalizeTourScreen.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, SafeAreaView, View, Text } from 'react-native';
+import { Image, TouchableOpacity, StyleSheet, SafeAreaView, View, Text } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
 import { NavigationContainer, useNavigation } from '@react-navigation/native';
 import Button from '../components/button'
 import firebase from 'firebase'
@@ -10,12 +11,55 @@ import DrawerHeader from '../components/drawerHeader'
 const Drawer = createDrawerNavigator();
 
 const FinalizeTourScreen = props => {
+    var default_image = require('../assets/default_thumbnail.png');
     const navigation = useNavigation();
+    const [selectedImage, setSelectedImage] = React.useState(null);
+
+
+    let openImagePickerAsync = async () => {
+        let permissionResult = await ImagePicker.requestCameraRollPermissionsAsync();
+        if (permissionResult.granted === false) {
+            alert("Permission to access camera roll is required!");
+            return;
+        }
+        let pickerResult = await ImagePicker.launchImageLibraryAsync();
+        console.log(pickerResult);
+        if (pickerResult.cancelled === true) {
+            return;
+        }
+
+        setSelectedImage({ localUri: pickerResult.uri });
+    };
+
+    const setImageToDefault = () => {
+        setSelectedImage(null);
+    };
+
+    const no_img_selected = <Image 
+                                source={default_image} 
+                                style={styles.thumbnail} 
+                            />
 
     return (
       <SafeAreaView style={styles.container}>
         <DrawerHeader name="Finalize Tour" openDrawer={props.navigation.openDrawer}/>
             <View style={styles.internalContainer}>
+                <View style={styles.container}>
+                    <Text style={styles.text}>Select a photo for your tour or use the default image.</Text>
+                </View>
+                <View style={styles.container}>
+                    <Text style={styles.text}>Currently selected image:</Text>
+                    {selectedImage !== null ? 
+                        <Image 
+                            source={{uri: selectedImage.localUri}} 
+                            style={styles.thumbnail} 
+                        /> : no_img_selected}
+                    <Button title="Pick a photo" onPress={openImagePickerAsync} />
+                    <Button title="Use default" onPress={setImageToDefault} />
+                </View>
+                <View style={styles.container}>
+                    <Button title="Finish tour" />
+                </View>
             </View>
       </SafeAreaView>
     
@@ -32,7 +76,7 @@ const styles = StyleSheet.create({
   internalContainer: {
       height: '100%',
       alignItems: 'center',
-      justifyContent: 'center',
+      justifyContent: 'flex-start',
       },
   header:{
       width:"100%",
@@ -51,8 +95,15 @@ const styles = StyleSheet.create({
       elevation: 2,
       width: 350,
   },
-  title: {
-      fontSize: 32,
+  text: {
+    fontSize: 16,
+  },
+  thumbnail: {
+    width: 300,
+    height: 300,
+    resizeMode: "contain",
+    marginTop: 10,
+    marginBottom: 10,
   },
 });
 
