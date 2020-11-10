ba265bcfd98401c68e5f34e0b1e863ad0a5dc6ca
Connor <connorbadams@gmail.com>
2020-11-10 15:45:40 -0600

    Navigation fix
    For some reason when merging a bug was introduced on the app.js navigator

--- PersonalizedCampus/App.js
+++ PersonalizedCampus/App.js
@@ -3,9 +3,12 @@ import { StyleSheet, Button, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator, CardStyleInterpolators } from '@react-navigation/stack';
 import HomeScreen from './screens/homeScreen'
+import TourTypeScreen from './screens/tourTypeScreen'
 import LoginScreen from './screens/loginScreen'
 import AccountCreationScreen from './screens/accountCreationScreen';
 import MapScreen from './screens/mapScreen';
+import SignedInScreen from './screens/signedInScreen';
+
 import * as firebase from 'firebase';
 import firebaseConfig from './config';
 
@@ -23,7 +26,9 @@ export default function App() {
       <Stack.Screen name="Home" component={HomeScreen} />
       <Stack.Screen name="Map" component={MapScreen} />
       <Stack.Screen name="Login" component={LoginScreen} />
+      <Stack.Screen name="Tour Type" component={TourTypeScreen} /> 
       <Stack.Screen name="Account Creation" component={AccountCreationScreen} />
+      <Stack.Screen name="SignedIn" component={SignedInScreen} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
